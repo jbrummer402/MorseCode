c524399761e7deb3069101dd3745b91941f0f90c
Jack Brummer <jbrummer@stevens.edu>
2019-12-15 15:06:57 -0500

    morse code cpp

---
+++ morsecode.cpp
@@ -0,0 +1,27 @@
+#include <vector>
+#include <algorithm>
+#include <iostream>
+
+
+using namespace std;
+
+vector<string> word;
+
+struct Node{
+    string data;
+    Node *left, *right;
+
+};
+
+struct Heap{
+    void insert();
+};
+
+Heap::insert(){
+    
+}
+
+int main(int argc, char *argv[]){
+
+    return 0;
+}
